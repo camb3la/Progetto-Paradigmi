7878ee0a29992859edc240a78df3eda924c52e25
camb3la <59169021+camb3la@users.noreply.github.com>
2024-08-23 16:29:44 +0200

    Aggiungere i file di progetto.

---
+++ Paradigmi.Models/Entities/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paradigmi.Models.Entities
+{
+    internal class Categoria
+    {
+        public int IdCategoria { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        
+    }
+}
